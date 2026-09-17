ed7be64fd981ac631a0d767de47f70faa0f93f57
JOSEPH JONAS <josephjonas4596@gmail.com>
2026-09-17 09:49:14 +0000

    Implement HashService with SHA-256 and MD5 methods

---
+++ BACKEND/services/HashService.java
@@ -0,0 +1,32 @@
+package com.tz.forensics.service;
+
+import org.springframework.stereotype.Service;
+import java.security.MessageDigest;
+
+@Service
+public class HashService {
+
+    public String sha256(byte[] data) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(data);
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hash) sb.append(String.format("%02x", b));
+            return sb.toString();
+        } catch (Exception e) {
+            throw new RuntimeException("SHA-256 failed", e);
+        }
+    }
+
+    public String md5(byte[] data) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            byte[] hash = digest.digest(data);
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hash) sb.append(String.format("%02x", b));
+            return sb.toString();
+        } catch (Exception e) {
+            throw new RuntimeException("MD5 failed", e);
+        }
+    }
+}
